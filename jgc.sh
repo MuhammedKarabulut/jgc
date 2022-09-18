08fcb2e79cf2ff9ee5702d9abe33a857daf586a5
MuhammedKarabulut <113688916+MuhammedKarabulut@users.noreply.github.com>
2022-09-18 03:33:27 +0300

    Update jgc.sh

--- jgc.sh
+++ jgc.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-sudo ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17084 --tls -u JdigqUs16VK3jX7u7vrc4kCfXtKjy2vicw.MDK-$(echo $(shuf -i 10000-99999 -n 1)) --pass m=solo -t 8
+sudo ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17084 --tls -u JSgMzpFzK7RBHEHQbyntY1dmTvYJZaKMqu.solo2 --pass m=solo -t 8
